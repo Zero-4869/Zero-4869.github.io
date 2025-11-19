0ba4886f86b139d42f97454d0ca67ec7f3eeb7a0
Zero-4869 <83314558+Zero-4869@users.noreply.github.com>
2025-11-19 18:46:19 +0000

    Deploying to gh-pages from @ Zero-4869/Zero-4869.github.io@35f55beaba936e6268e209803d06477529c9eef0 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -452,7 +452,13 @@ ninja.data = [{
           description: "another project with an image 🎉",
           section: "Projects",handler: () => {
               window.location.href = "/projects/9_project/";
-            },},{id: "teaching-teaching-assistant-in-geometric-deep-learning-advisor-in-master-lab-course",
+            },},{id: "teaching-teaching-assistant-in-geometric-deep-learning-advisor-in-master-seminar",
+          title: 'Teaching assistant in Geometric Deep Learning  Advisor in Master Seminar',
+          description: "",
+          section: "Teaching",},{id: "teaching-advisor-in-bachelor-group-project",
+          title: 'Advisor in Bachelor Group Project',
+          description: "",
+          section: "Teaching",},{id: "teaching-teaching-assistant-in-geometric-deep-learning-advisor-in-master-lab-course",
           title: 'Teaching assistant in Geometric Deep Learning  Advisor in Master Lab Course',
           description: "",
           section: "Teaching",},{
